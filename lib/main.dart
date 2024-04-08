5526d5c6205d2b24ce93f0e2c7be8d85d0c66e37
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-04-09 01:14:28 +0900

    feat : 가로모드 고정

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,21 +28,44 @@ class SensorApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations(
+      [
+        DeviceOrientation.landscapeLeft,
+      ],
+    );
     final centerX = MediaQuery.of(context).size.width / 2 - 50;
     final centerY = MediaQuery.of(context).size.height / 2 - 50;
 
     return Scaffold(
       body: Stack(
         children: [
-          Positioned(
-            left: centerX,
-            top: centerY,
-            child: Container(
-              color: Colors.red,
-              width: 100,
-              height: 100,
-            ),
-          ),
+          StreamBuilder<AccelerometerEvent>(
+              stream: accelerometerEventStream(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+
+                final event = snapshot.data!;
+
+                List<double> accelerometerValues = [event.x, event.y, event.z];
+                print(accelerometerValues);
+
+                return Positioned(
+                  left: centerX,
+                  top: centerY,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.green[100],
+                    ),
+                    width: 100,
+                    height: 100,
+                  ),
+                );
+              }),
         ],
       ),
     );
