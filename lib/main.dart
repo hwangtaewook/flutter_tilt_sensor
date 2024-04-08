82dc1ae2b2d9552b28818c317f3370a827003b4e
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-04-09 00:53:07 +0900

    feat : Stack, Positioned, MediaQuery

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -25,6 +26,23 @@ class SensorApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final centerX = MediaQuery.of(context).size.width / 2 - 50;
+    final centerY = MediaQuery.of(context).size.height / 2 - 50;
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            left: centerX,
+            top: centerY,
+            child: Container(
+              color: Colors.red,
+              width: 100,
+              height: 100,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
